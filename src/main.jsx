6f7d58c0a12166acb233756a9a135f51cee53d40
Qazaq009 <nurbol1990.19@gmail.com>
2025-04-15 13:42:38 +0500

    Fix: заменён Layout на App в main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./main"; // 👈 подключаем Layout, который содержит все маршруты
+import App from "./App"; // ✅ Вместо "./main"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
